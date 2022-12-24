ee9ac377a760864282fbc91627dfe5a45c9da20f
f4faysal <f4faysals@gmai.com.com>
2022-12-24 16:05:34 +0600

    fierbase login

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKVg9iii2t2AgcHsxRk3z4DROE10_p1ZY",
+  authDomain: "techtocloud-app.firebaseapp.com",
+  projectId: "techtocloud-app",
+  storageBucket: "techtocloud-app.appspot.com",
+  messagingSenderId: "1000467727962",
+  appId: "1:1000467727962:web:53487288d45e08cec2ba63",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
